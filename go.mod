b8f67d04c9402e2e0c2cf3b015271bdf0e322b18
Istiak <istiakurrahman50@gmail.com>
2025-07-11 18:49:30 +0600

    update

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module go_book_management
-
 go 1.24
-
-require github.com/go-chi/chi/v5 v5.2.2 // indirect
+require github.com/go-chi/chi/v5 v5.2.2
--- handlers/book.go
+++ handlers/book.go
@@ -1,8 +1,8 @@
 package handlers
 
 import (
-	_ "fmt"
 	"encoding/json"
+	_ "fmt"
 	"github.com/go-chi/chi/v5"
 	"go_book_management/models"
 	"go_book_management/storage"
