8c839ac2a864fcc2ef7ec9a257ac1a1665b0da83
Istiak <istiakurrahman50@gmail.com>
2025-07-11 16:42:00 +0600

    add README

---
+++ README.md
@@ -0,0 +1,12 @@
+## 📘 API Endpoints
+
+| Method | Endpoint          | Description               |
+|--------|-------------------|---------------------------|
+| GET    | `/books`          | Retrieve all books        |
+| GET    | `/books/{id}`     | Retrieve a book by ID     |
+| POST   | `/books`          | Add a new book            |
+| PUT    | `/books/{id}`     | Update an existing book   |
+| DELETE | `/books/{id}`     | Delete a book             |
+| GET    | `/find/{genre}`   | Search books by genre     |
+| GET    | `/authors`        | Retrieve all authors      |
+| GET    | `/authors/{id}`   | Retrieve an author by ID  |
--- storage/db.go
+++ storage/db.go
@@ -6,8 +6,8 @@ var Books = make(map[string]models.Book)
 var Authors = make(map[string]models.Author)
 
 func Init() {
-	Books["1"] = models.Book{ID: "1", Title: "Movie1", Genre: "Comedy", Author: &models.Author{ID: "1", FirstName: "Istaikur", LastName: "Rahman"}}
-	Books["2"] = models.Book{ID: "2", Title: "Movie2", Genre: "Comedy", Author: &models.Author{ID: "2", FirstName: "Pulok", LastName: "Saha"}}
+	Books["1"] = models.Book{ID: "1", Title: "Book1", Genre: "Comedy", Author: &models.Author{ID: "1", FirstName: "Istaikur", LastName: "Rahman"}}
+	Books["2"] = models.Book{ID: "2", Title: "Book2", Genre: "Comedy", Author: &models.Author{ID: "2", FirstName: "Pulok", LastName: "Saha"}}
 
 	Authors["1"] = models.Author{ID: "1", FirstName: "Istaikur", LastName: "Rahman"}
 	Authors["2"] = models.Author{ID: "2", FirstName: "Pulok", LastName: "Saha"}
